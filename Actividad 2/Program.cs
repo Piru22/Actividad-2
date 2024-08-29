f0fbff4287360eaf2bb19a67c3fe6bcefe50939e
Navegador <Navegador@RLG10728-70981>
2024-08-29 11:22:04 -0300

    Envio cambios

--- Actividad 2/Program.cs
+++ Actividad 2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Actividad_2
 {
@@ -10,6 +12,32 @@ namespace Actividad_2
     {
         static void Main(string[] args)
         {
+            string connectionString = "Data Source=localhost;" +
+                "initial catalog=Actividad 2;" +
+                "Integrated Security = true;";
+
+            String query = "SELECT * FROM Profesores";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0},{1},{2}",
+                        reader[0], reader[1], reader[2]);
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
